df8b0880f2ec7a7ee8e0337c5ad015ecf35b506a
juanpciceri <32818490+juanpciceri@users.noreply.github.com>
2021-02-23 10:35:33 -0500

    Update boto3.py
    Give better performance and let filter EBS snapshots according to tags

--- boto3.py
+++ boto3.py
@@ -4,20 +4,38 @@ def lambda_handler(event, context):
     volume_iterator = ec2.volumes.all()
     name = None
     for v in volume_iterator:
-        #print(v)
-        keys=[]
-        for tag in v.tags:
         
-            h=tag['Key']
-            keys.append(h)
+        keys=[]
+        if v.tags is not None:
+            for tag in v.tags:
+        
+                h=tag['Key']
+                keys.append(h)
+            
              
         
-        print(keys)
-        if 'volume_type' not in keys:
-            print(v)
+        #print(keys)
+            if 'deployment_guid' or 'lm_troux_uid' not in keys:
+                print(v.volume_id)
                  
+          
+    
+    ec2client = boto3.client('ec2')
+    response = ec2client.describe_snapshots(
+       Filters=[
+            {
+                'Name': 'tag-key',
+               'Values': ["deployment_guid"]
+            },
             
-        keys=[]     
-            
-           
-        
+       ]
+    )
+  
+   
+    dataList = response['Snapshots']
+    index = 0
+    while index < len(dataList):
+        for key in dataList[index]:
+            if key=='SnapshotId':
+                print(dataList[index][key])
+        index += 1
